786397195445e454815ab6d5515842619cfc50e8
Raul <raulfabiano.dev@gmail.com>
2025-05-26 17:03:01 -0300

    feat: colocando a navbar o footer no app.vue

--- Portifolio/src/App.vue
+++ Portifolio/src/App.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="min-h-screen flex flex-col justify-between bg-gray-50">
     <main class="flex-1">
+      <NavBarComponents />
       <router-view />
+      <FooterComponents/>
     </main>
   </div>
 </template>
 
 <script setup>
+import NavBarComponents from './components/NavBarComponents.vue'
+import FooterComponents from './components/FooterComponents.vue'
 </script>
