ac4eb5b1e6785e329d302fd311124d3aa29dcb74
Raul <raulfabiano.dev@gmail.com>
2025-05-26 17:12:27 -0300

    feat: finalizando navbar e footer

---
+++ Portifolio/src/components/FooterComponents.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer class="bg-[#071427] text-white text-center py-4 shadow-inner">
+    <p class="text-sm">&copy; {{ new Date().getFullYear() }} Portfólio - Todos os direitos reservados</p>
+  </footer>
+</template>
